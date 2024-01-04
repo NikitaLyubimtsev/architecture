593188495b5426324705845fcd3a6a2608e562f2
NikitaAlexandrovich <LyubimtsevN.A@yandex.ru>
2024-01-04 20:48:42 +0300

    init

--- src/main/kotlin/inMemoryModel/ModelStore.kt
+++ src/main/kotlin/inMemoryModel/ModelStore.kt
@@ -1,17 +1,29 @@
 package inMemoryModel
 
-import modelElements.Camera
-import modelElements.Flash
-import modelElements.PoligonalModel
-import modelElements.Scene
+import Staff.Color
+import modelElements.*
 
 class ModelStore(
     private var changedObserver: Array<IModelChangedObserver>
 ) : IModelChanger {
 
-    var models = mutableListOf(PoligonalModel())
-    var scenes = mutableListOf(Scene())
-    var flashes = mutableListOf(Flash())
+    var models = mutableListOf(PoligonalModel(listOf(Texture())))
+    var scenes = mutableListOf(Scene(
+        id = 1,
+        models = listOf(PoligonalModel(listOf(Texture()))),
+        flashes = listOf(
+            Flash(
+                power = 2.34F,
+                color = Color()
+            )
+        )
+    ))
+    var flashes = mutableListOf(
+        Flash(
+            power = 1.23F,
+            color = Color()
+        )
+    )
     var cameras = mutableListOf(Camera())
 
     override fun notifyChange(model: IModelChanger) {
